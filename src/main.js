47171c6dc2cddcf9b7d453e727a8402b43f3e292
Tiffany Siu <tsiu88@gmail.com>
2020-02-14 10:54:41 -0800

    add html output and fix api name output

--- src/main.js
+++ src/main.js
@@ -37,9 +37,14 @@ $(document).ready(function(){
       if (count === 0){
         $("#results").append("No doctors that met the inputted criteria were found.");
       } else{
-        $("#results").append(`<ol id="doctors"></ol>`);
+        $("#results").append(`<ol type="1" id="doctors"></ol>`);
         for (let i =0; i < count; i++){
-          $("#results").append(`<li id="doctor${i}"> ${response.data.practices[i].name}</li>`);
+          let middleName = "";
+          if(response.data[i].profile.middle_name != undefined){
+            middleName = response.data[i].profile.middle_name;
+          }
+          let fullName = response.data[i].profile.first_name + " " + middleName + " " + response.data[i].profile.last_name
+          $("#doctors").append(`<li id="doctor${i}"> ${fullName}</li>`);
         }
       }
       
